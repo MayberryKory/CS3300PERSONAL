fdce39d40dba81421ba8e4c5396c3448b66c8df1
MayberryKory <102997911+MayberryKory@users.noreply.github.com>
2023-09-16 11:32:53 -0600

    update views

--- portfolio/portfolio_app/views.py
+++ portfolio/portfolio_app/views.py
@@ -6,4 +6,4 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     #render HTML template index.html with the data in the variable
-    return HttpResponse('home page')
+    return render ( request, 'portfolio_app/index.html')
