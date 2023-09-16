44007114c2fbe7d94c9bec740fafaab3d3ae6163
MayberryKory <102997911+MayberryKory@users.noreply.github.com>
2023-09-16 10:49:40 -0600

    Define URI path and view

--- portfolio/portfolio_app/views.py
+++ portfolio/portfolio_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+
 
 # Create your views here.
+def index(request):
+    #render HTML template index.html with the data in the variable
+    return HttpResponse('home page')
